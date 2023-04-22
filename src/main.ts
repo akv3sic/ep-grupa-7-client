bc1d2c9eb82f875ba1b8f6a10c2c3766feeea9d8
akv3sic <antekvesic2@gmail.com>
2023-04-22 20:52:53 +0200

    #5 add tailwind and primevue

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,23 @@ import router from './router'
 
 import './assets/main.css'
 
+//primevue
+import PrimeVue from 'primevue/config';
+
+//theme
+import "primevue/resources/themes/tailwind-light/theme.css";   
+    
+//core
+import "primevue/resources/primevue.min.css";
+
+//icons
+import "primeicons/primeicons.css";
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+app.use(PrimeVue);
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,35 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="flex flex-col bg-blue-200 p-5">
+      <h1 class="my-5">This is Tailwind and PrimeVue test</h1>
+      <Button label="Test">
+      </Button>
+      <div class="flex flex-row mb-3 mt-6 justify-between px-12">
+        <i class="pi pi-check"></i>
+        <i class="pi pi-times"></i>
+        <span class="pi pi-search"></span>
+        <span class="pi pi-user"></span>
+      </div>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Button from 'primevue/button';
+
+export default defineComponent({
+  name: 'AboutView',
+  setup() {
+    return {}
+  },
+  components: {
+    Button
+  }
+
+
+})
+</script>
 
 <style>
 @media (min-width: 1024px) {
