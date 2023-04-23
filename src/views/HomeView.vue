bf64d3f6c4754c27ee0c9761bd1d0e482965dce0
akv3sic <antekvesic2@gmail.com>
2023-04-23 19:23:26 +0200

    #6 remove default styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>Home</h1>
+    <p>
+      This is the home page.
+    </p>
   </main>
 </template>
+
+<script setup lang="ts">
+
+</script>
