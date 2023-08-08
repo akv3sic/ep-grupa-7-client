93f6d106fe102619d8a53283642896a6ab071dff
akv3sic <antekvesic2@gmail.com>
2023-08-08 13:51:17 +0200

    #29 add icons to navigation items

--- src/components/layout/TheNavigationDrawer.vue
+++ src/components/layout/TheNavigationDrawer.vue
@@ -1,65 +1,66 @@
 <template>
-    <div class="bg-gray-100 shadow-md w-64 flex-shrink-0 h-full hidden lg:block fixed lg:static z-50"
-      :class="[isCollapsed ? '!hidden' : '!block']">
-      <!-- logo -->
-      <router-link to="/">
-        <div class="flex items-center justify-center h-16">
+  <div class="bg-gray-100 shadow-md w-64 flex-shrink-0 h-full hidden lg:block fixed lg:static z-50"
+    :class="[isCollapsed ? '!hidden' : '!block']">
+    <!-- logo -->
+    <router-link to="/">
+      <div class="flex items-center justify-center h-16">
         <img src="@/assets/img/fsre-logo.svg" alt="logo" class="h-8" />
       </div>
-      </router-link>
-      
-  
-      <!-- navigation -->
-      <nav class="mt-5">
-        <ul class="space-y-1">
-          <template v-for="(item, index) in navigationItems" :key="index">
-            <li class="mx-2">
-              <router-link :to="item.link" active-class="bg-gray-300 text-gray-900"
-                class="block py-2 px-4 text-gray-800 hover:bg-gray-200 hover:text-gray-900 rounded-md">
-                <span class="font-medium text-sm">
-                  {{ item.label }}
-                </span>
-              </router-link>
-            </li>
-          </template>
-        </ul>
-      </nav>
-    </div>
-  </template>
-  <style scoped>
-  main {
-    padding: 1rem;
-  }
-  </style>
-  <script lang="ts">
-  import { defineComponent } from 'vue';
-  
-  export default defineComponent({
-    name: 'NavigationDrawer',
-    setup() {
-      const navigationItems = [
-        { label: 'Pregled', link: '/' },
-        { label: 'Zaposlenici', link: '/zaposlenici' },
-        { label: 'Radna mjesta', link: '/radna-mjesta' },
-        { label: 'Odjeli', link: '/odjeli' },
-        { label: 'Radni centri', link: '/radni-centri' },
-        { label: 'Radni nalozi', link: '/radni-nalozi' },
-        { label: 'Kvarovi', link: '/kvarovi' },
-        { label: 'Postavke', link: '/postavke' },
-      ]
-  
+    </router-link>
 
-      return {
-        navigationItems,
-      }
+
+    <!-- navigation -->
+    <nav class="mt-5">
+      <ul class="space-y-1">
+        <template v-for="(item, index) in navigationItems" :key="index">
+          <li class="mx-2">
+            <router-link :to="item.link" active-class="bg-gray-300 text-gray-900"
+              class="block py-2 px-4 text-gray-800 hover:bg-gray-200 hover:text-gray-900 rounded-md flex items-center">
+              <i class="mr-2" :class="item.icon"></i> <!-- added icon -->
+              <span class="font-medium text-sm">
+                {{ item.label }}
+              </span>
+            </router-link>
+          </li>
+        </template>
+      </ul>
+    </nav>
+  </div>
+</template>
+<style scoped>
+main {
+  padding: 1rem;
+}
+</style>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'NavigationDrawer',
+  setup() {
+    const navigationItems = [
+      { label: 'Pregled', link: '/', icon: 'pi pi-home' },
+      { label: 'Zaposlenici', link: '/zaposlenici', icon: 'pi pi-user' },
+      { label: 'Radna mjesta', link: '/radna-mjesta', icon: 'pi pi-briefcase' },
+      { label: 'Odjeli', link: '/odjeli', icon: 'pi pi-building' },
+      { label: 'Radni centri', link: '/radni-centri', icon: 'pi pi-sitemap' },
+      { label: 'Radni nalozi', link: '/radni-nalozi', icon: 'pi pi-folder' },
+      { label: 'Kvarovi', link: '/kvarovi', icon: 'pi pi-wrench' },
+      { label: 'Postavke', link: '/postavke', icon: 'pi pi-cog' },
+    ]
+
+
+    return {
+      navigationItems,
+    }
+  },
+  props: {
+    isCollapsed: {
+      type: Boolean,
+      required: true,
     },
-    props: {
-      isCollapsed: {
-        type: Boolean,
-        required: true,
-      },
-    },
-    components: {
-    },
-  });
-  </script> 
+  },
+  components: {
+  },
+});
+</script> 
