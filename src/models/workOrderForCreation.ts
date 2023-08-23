da8d3cef82a7dfd2be519d6ebe08e97245765155
akv3sic <antekvesic2@gmail.com>
2023-08-23 23:11:40 +0200

    #82 Implement "Add New Work Order" Feature

---
+++ src/models/workOrderForCreation.ts
@@ -0,0 +1,8 @@
+export interface WorkOrderForCreation {
+    title: string;
+    description: string;
+    due_time: string;
+    work_center: number;
+    category: number;
+    assigned_to: number;
+}
--- src/stores/workOrders.store.ts
+++ src/stores/workOrders.store.ts
@@ -2,6 +2,7 @@ import httpClient from "@/common/httpClient";
 import { defineStore } from "pinia";
 import { ref } from "vue";
 import type { WorkOrder } from "@/models/workOrder.model";
+import type { WorkOrderForCreation } from "@/models/workOrderForCreation";
 
 export const useWorkOrdersStore = defineStore("workOrders", () => {
     const workOrders = ref<WorkOrder[]>([]);
@@ -28,9 +29,30 @@ export const useWorkOrdersStore = defineStore("workOrders", () => {
         }
     };
 
+    const addWorkOrder = async (workOrder: WorkOrderForCreation) => {
+        isLoading.value = true;
+        try {
+            const response = await httpClient.post("/work-orders/", workOrder);
+            if (response.status === 201 || response.status === 200) { // 201 - Created, 200 - OK
+                workOrders.value.push(response.data);
+            } else {
+                error.value = "Greška pri dodavanju radnog naloga";
+            }
+        } catch (err: any) {
+            if (err instanceof Error) {
+                error.value = err.message;
+            } else {
+                error.value = "Greška pri dodavanju radnog naloga";
+            }
+        } finally {
+            isLoading.value = false;
+        }
+    };
+
     return {
         workOrders,
         fetchWorkOrders,
+        addWorkOrder,
         isLoading,
         error
     };
